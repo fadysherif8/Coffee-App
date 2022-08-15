0b3df87ff1e691641cf882de5f6a2abccd60b0e1
Fady sherif <fadysherif8@icloud.com>
2022-08-16 00:44:07 +0200

    changed some files

---
+++ CoffeeApp/newData/CoffeeListModel.swift
@@ -0,0 +1,19 @@
+//
+//  CoffeeListModel.swift
+//  CoffeeApp
+//
+//  Created by Fady on 16/08/2022.
+//
+
+import Foundation
+import UIKit
+
+class Coffee{
+    var title: String
+    var image: UIImage
+    
+    init(title: String, image: UIImage){
+        self.title = title
+        self.image = image
+    }
+}
